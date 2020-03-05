8a56255e28c5e29679cb122d3656d624ec2eb86b
leandro-gft <59618736+leandro-gft@users.noreply.github.com>
2020-03-05 14:52:21 -0300

    Update CasaService
    Verificação se o nome da casa já existe antes de cadastrar.

--- src/main/java/br/com/gft/casadeshowapi/service/CasaService.java
+++ src/main/java/br/com/gft/casadeshowapi/service/CasaService.java
@@ -18,52 +18,60 @@ public class CasaService {
 
 	@Autowired
 	private CasaRepository casasRepository;
-	
-	
+
 	public List<Casa> listar() {
 		return casasRepository.findAll();
 	}
-	
+
 	public List<Casa> listarasc() {
 		Sort sort = Sort.by("nomeCasa").ascending();
 		return casasRepository.findAll(sort);
 	}
-	
+
 	public List<Casa> listardesc() {
 		Sort sort = Sort.by("nomeCasa").descending();
 		return casasRepository.findAll(sort);
 	}
-	
+
 	public Casa salvar(Casa casa) {
-		if (casa.getId() !=null && casa.getNomeCasa() !=null ) {
-			Casa a  = casasRepository.findById(casa.getId()).orElse(null);
-			Casa b = casasRepository.findByNomeCasa(casa.getNomeCasa());
-			if (a  != null || b != null ) //se a for diferente de null, significa que foi encontrado no banco, ou seja, já existe
+		if (casa.getId() != null) {
+			Casa a = casasRepository.findById(casa.getId()).orElse(null);
+			if (a != null) // se a for diferente de null, significa que foi encontrado no banco, ou seja,
+										// já existe
+			{
+				throw new CasaExistenteException("Essa casa já existe.");
+			}
+		} else {
+			if (casa.getNomeCasa() != null) {
+				Casa b = casasRepository.findByNomeCasa(casa.getNomeCasa());
+				if (b != null) // se a for diferente de null, significa que foi encontrado no banco, ou seja,
+								// já existe
 				{
-				throw new CasaExistenteException("Essa casa já existe.");				
-			}			
+					throw new CasaExistenteException("Essa casa já existe.");
+				}
+			}
 		}
 		return casasRepository.save(casa);
 	}
-	
+
 	public Casa buscar(Long id) {
 		Casa casa = casasRepository.findById(id).orElse(null);
-		
-		if(casa ==null) {
+
+		if (casa == null) {
 			throw new EmptyResultDataAccessException(1);
-			
+
 		}
-		return casa;		
+		return casa;
 	}
-	
+
 	public Casa buscarPorNome(String nomeCasa) {
 		Casa casa = casasRepository.findByNomeCasa(nomeCasa);
-		
-		if(casa ==null) {
+
+		if (casa == null) {
 			throw new EmptyResultDataAccessException(1);
-			
+
 		}
-		return casa;		
+		return casa;
 	}
 
 	public void deletar(Long id) {
@@ -83,4 +91,5 @@ public class CasaService {
 	private void verificarExistencia(Casa casa) {
 		buscar(casa.getId());
 	}
+
 }
