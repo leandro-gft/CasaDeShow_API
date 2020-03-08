601a344d6910e8065e035338d32b8b191f509f44
leandro-gft <59618736+leandro-gft@users.noreply.github.com>
2020-03-08 12:07:52 -0300

    Configuracao CORS
    Implementação de um filtro CORS para que a aplicação aceite requisições de origens diferente ao localhost.

--- src/main/java/br/com/gft/casadeshowapi/resource/EventoResource.java
+++ src/main/java/br/com/gft/casadeshowapi/resource/EventoResource.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 //import org.springframework.security.core.Authentication;
 //import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -33,7 +34,7 @@ public class EventoResource {
 
 	@Autowired
 	private EventoService eventosService;
-
+	
 	@ApiOperation("Lista os eventos cadastrados.")
 	@GetMapping
 	public ResponseEntity<List<Evento>> Listar() { 
