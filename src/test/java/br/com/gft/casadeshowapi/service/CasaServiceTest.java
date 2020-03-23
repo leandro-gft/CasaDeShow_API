782f8d1954131095cd2797b286c7aa36bb0700fb
leandro-gft <59618736+leandro-gft@users.noreply.github.com>
2020-03-23 12:28:15 -0300

    Update CasaServiceTest.java

--- src/test/java/br/com/gft/casadeshowapi/service/CasaServiceTest.java
+++ src/test/java/br/com/gft/casadeshowapi/service/CasaServiceTest.java
@@ -23,28 +23,40 @@ import br.com.gft.casadeshowapi.repository.CasaRepository;
 public class CasaServiceTest {
 	
 	@Autowired
-	private CasaRepository casasRepository;
+	private CasaService service;
 	
 	@Test
 	public void deveMostrarOTamanhoDaListaDeCasasDeShow() {
-		List<Casa> casas = casasRepository.findAll();
+		List<Casa> casas = service.listar();
 		assertThat(casas.size()).isEqualTo(7);
 	}
 	
 	@Test
 	public void deveProcurarPorNomeDaCasaDeShow() {
-		List<Casa> casa = casasRepository.findByNomeCasaContaining("Teatro");
+		List<Casa> casa = service.buscarPorNome("Teatro");
 		assertEquals(casa.get(0).getNomeCasa(), "Teatro Municipal");
 	}
 	
 	@Test
 	public void deveBuscarCasaDeShowPorId() {
 		//RequestEntity<Void> request = RequestEntity.get(URI.create("http://localhost:8080/api/casas/")).header("Authorization", "Basic TGVhbmRybzoxMjM=").build();
-		Optional<Casa> casa = casasRepository.findById((long) 1);
-		assertEquals(casa.get().getNomeCasa(), "Teatro Municipal");
+		Casa casa = service.buscar((long) 1);
+		assertEquals(casa.getNomeCasa(), "Teatro Municipal");
 	}
 	
-	
+	@Test
+	public void deveValidarOPrimeiroDaListaEmOrdemCrescente() {
+		//RequestEntity<Void> request = RequestEntity.get(URI.create("http://localhost:8080/api/casas/")).header("Authorization", "Basic TGVhbmRybzoxMjM=").build();
+		List<Casa> casa = service.listarasc();
+		assertEquals(casa.get(0).getNomeCasa(), "abc");
+	}
 
+	@Test
+	public void deveValidarOPrimeiroDaListaEmOrdemDecrescente() {
+		//RequestEntity<Void> request = RequestEntity.get(URI.create("http://localhost:8080/api/casas/")).header("Authorization", "Basic TGVhbmRybzoxMjM=").build();
+		List<Casa> casa = service.listardesc();
+		assertEquals(casa.get(0).getNomeCasa(), "Teatro Municipal");
+	}
+		
 
 }
